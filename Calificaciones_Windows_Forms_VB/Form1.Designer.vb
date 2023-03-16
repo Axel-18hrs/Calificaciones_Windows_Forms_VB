06a8647e23a55e6c7916f0ea36ee1dc5b92beb21
Axel Gael Carlin Guzman <axel.18hrs@gmail.com>
2023-03-15 23:18:12 -0600

    Agregar archivos de proyecto.

---
+++ Calificaciones_Windows_Forms_VB/Form1.Designer.vb
@@ -0,0 +1,334 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.nud1 = New System.Windows.Forms.NumericUpDown()
+        Me.nud2 = New System.Windows.Forms.NumericUpDown()
+        Me.nud3 = New System.Windows.Forms.NumericUpDown()
+        Me.nud4 = New System.Windows.Forms.NumericUpDown()
+        Me.nud5 = New System.Windows.Forms.NumericUpDown()
+        Me.nud6 = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.dgvCalificaciones = New System.Windows.Forms.DataGridView()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.btnEnviar = New System.Windows.Forms.Button()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.nud1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nud2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nud3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nud4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nud5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nud6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvCalificaciones, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'nud1
+        '
+        Me.nud1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nud1.Location = New System.Drawing.Point(257, 92)
+        Me.nud1.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.nud1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.nud1.Name = "nud1"
+        Me.nud1.ReadOnly = True
+        Me.nud1.Size = New System.Drawing.Size(191, 32)
+        Me.nud1.TabIndex = 0
+        Me.nud1.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'nud2
+        '
+        Me.nud2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nud2.Location = New System.Drawing.Point(257, 152)
+        Me.nud2.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.nud2.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.nud2.Name = "nud2"
+        Me.nud2.ReadOnly = True
+        Me.nud2.Size = New System.Drawing.Size(191, 32)
+        Me.nud2.TabIndex = 1
+        Me.nud2.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'nud3
+        '
+        Me.nud3.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nud3.Location = New System.Drawing.Point(257, 211)
+        Me.nud3.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.nud3.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.nud3.Name = "nud3"
+        Me.nud3.ReadOnly = True
+        Me.nud3.Size = New System.Drawing.Size(191, 32)
+        Me.nud3.TabIndex = 2
+        Me.nud3.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'nud4
+        '
+        Me.nud4.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nud4.Location = New System.Drawing.Point(257, 269)
+        Me.nud4.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.nud4.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.nud4.Name = "nud4"
+        Me.nud4.ReadOnly = True
+        Me.nud4.Size = New System.Drawing.Size(191, 32)
+        Me.nud4.TabIndex = 3
+        Me.nud4.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'nud5
+        '
+        Me.nud5.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nud5.Location = New System.Drawing.Point(257, 326)
+        Me.nud5.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.nud5.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.nud5.Name = "nud5"
+        Me.nud5.ReadOnly = True
+        Me.nud5.Size = New System.Drawing.Size(191, 32)
+        Me.nud5.TabIndex = 4
+        Me.nud5.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'nud6
+        '
+        Me.nud6.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nud6.Location = New System.Drawing.Point(257, 382)
+        Me.nud6.Maximum = New Decimal(New Integer() {10, 0, 0, 0})
+        Me.nud6.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.nud6.Name = "nud6"
+        Me.nud6.ReadOnly = True
+        Me.nud6.Size = New System.Drawing.Size(191, 32)
+        Me.nud6.TabIndex = 5
+        Me.nud6.Value = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(34, 101)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(202, 23)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Activity 1 and unit 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(34, 161)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(202, 23)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Activity 2 and unit 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(34, 220)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(202, 23)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Activity 3 and unit 3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(34, 278)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(202, 23)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Activity 4 and unit 4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(34, 335)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(202, 23)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Activity 5 and unit 5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(34, 391)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(202, 23)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Activity 6 and unit 6"
+        '
+        'dgvCalificaciones
+        '
+        Me.dgvCalificaciones.AllowUserToAddRows = False
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.dgvCalificaciones.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.dgvCalificaciones.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvCalificaciones.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6})
+        Me.dgvCalificaciones.Location = New System.Drawing.Point(498, 98)
+        Me.dgvCalificaciones.Name = "dgvCalificaciones"
+        Me.dgvCalificaciones.RowHeadersVisible = False
+        Me.dgvCalificaciones.RowHeadersWidth = 51
+        Me.dgvCalificaciones.RowTemplate.Height = 24
+        Me.dgvCalificaciones.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvCalificaciones.Size = New System.Drawing.Size(757, 315)
+        Me.dgvCalificaciones.TabIndex = 12
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Century Gothic", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(236, 36)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(861, 34)
+        Me.Label7.TabIndex = 14
+        Me.Label7.Text = "Califica las unidades del estudiante que creo esta aplicacion"
+        '
+        'btnEnviar
+        '
+        Me.btnEnviar.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnEnviar.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEnviar.Location = New System.Drawing.Point(38, 435)
+        Me.btnEnviar.Name = "btnEnviar"
+        Me.btnEnviar.Size = New System.Drawing.Size(193, 66)
+        Me.btnEnviar.TabIndex = 15
+        Me.btnEnviar.Text = "Enviar"
+        Me.btnEnviar.UseVisualStyleBackColor = False
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Unit 1"
+        Me.Column1.MinimumWidth = 6
+        Me.Column1.Name = "Column1"
+        Me.Column1.ReadOnly = True
+        Me.Column1.Width = 125
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Unit 2"
+        Me.Column2.MinimumWidth = 6
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        Me.Column2.Width = 125
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Unit 3"
+        Me.Column3.MinimumWidth = 6
+        Me.Column3.Name = "Column3"
+        Me.Column3.ReadOnly = True
+        Me.Column3.Width = 125
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Unit 4"
+        Me.Column4.MinimumWidth = 6
+        Me.Column4.Name = "Column4"
+        Me.Column4.ReadOnly = True
+        Me.Column4.Width = 125
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Unit 5"
+        Me.Column5.MinimumWidth = 6
+        Me.Column5.Name = "Column5"
+        Me.Column5.ReadOnly = True
+        Me.Column5.Width = 125
+        '
+        'Column6
+        '
+        Me.Column6.HeaderText = "Unit 6"
+        Me.Column6.MinimumWidth = 6
+        Me.Column6.Name = "Column6"
+        Me.Column6.ReadOnly = True
+        Me.Column6.Width = 125
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1320, 529)
+        Me.Controls.Add(Me.btnEnviar)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.dgvCalificaciones)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.nud6)
+        Me.Controls.Add(Me.nud5)
+        Me.Controls.Add(Me.nud4)
+        Me.Controls.Add(Me.nud3)
+        Me.Controls.Add(Me.nud2)
+        Me.Controls.Add(Me.nud1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.nud1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nud2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nud3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nud4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nud5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nud6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvCalificaciones, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents nud1 As NumericUpDown
+    Friend WithEvents nud2 As NumericUpDown
+    Friend WithEvents nud3 As NumericUpDown
+    Friend WithEvents nud4 As NumericUpDown
+    Friend WithEvents nud5 As NumericUpDown
+    Friend WithEvents nud6 As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents dgvCalificaciones As DataGridView
+    Friend WithEvents Label7 As Label
+    Friend WithEvents btnEnviar As Button
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+End Class
---
+++ Calificaciones_Windows_Forms_VB/Form1.vb
@@ -0,0 +1,80 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private reader As BinaryReader
+    Private writer As BinaryWriter
+    Private readerStream As StreamReader
+    Private fileStream As FileStream
+    Private ofd As OpenFileDialog
+
+    Sub New()
+
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ofd = New OpenFileDialog()
+        ofd.Title = "Calificaciones del alumno"
+        ofd.Filter = "Archivos binarios (*.bin)|*.bin"
+
+        Dim opc As DialogResult = MessageBox.Show("Quieres usar un archivo existente?", "???", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+        If opc = DialogResult.Yes Then
+
+            If ofd.ShowDialog() = DialogResult.OK Then
+
+                fileStream = New FileStream(ofd.FileName, FileMode.Open, FileAccess.Read)
+                reader = New BinaryReader(fileStream)
+
+                While reader.BaseStream.Position < reader.BaseStream.Length
+                    Dim value1 As Double = reader.ReadDouble()
+                    Dim value2 As Double = reader.ReadDouble()
+                    Dim value3 As Double = reader.ReadDouble()
+                    Dim value4 As Double = reader.ReadDouble()
+                    Dim value5 As Double = reader.ReadDouble()
+                    Dim value6 As Double = reader.ReadDouble()
+                    dgvCalificaciones.Rows.Add(value1, value2, value3, value4, value5, value6)
+                    btnEnviar.Enabled = False
+                    MessageBox.Show("No se podra calificar nuevamente si ya se establecio un archivo para este programa", "???", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                End While
+                reader.Close()
+            Else
+                MessageBox.Show("Error al guardar el archivo", "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+
+        Dim x As DialogResult = MessageBox.Show("Deseas guardar las calificaciones?", "???", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation)
+        If x = DialogResult.Yes Then
+            Dim ruta As SaveFileDialog = New SaveFileDialog()
+            ruta.Filter = "Archivos binarios (*.bin)|*.bin"
+            ruta.Title = "Calificaciones del alumno"
+
+            If ruta.ShowDialog() = DialogResult.Cancel Then
+                MessageBox.Show("Error al escoger la ruta de archivo. ", "Error", MessageBoxButtons.OK)
+                Return
+            End If
+            Dim rutad As FileStream = New FileStream(ruta.FileName, FileMode.Create, FileAccess.Write)
+            writer = New BinaryWriter(rutad)
+            For Each row As DataGridViewRow In dgvCalificaciones.Rows
+                ' Itera sobre cada celda en la fila
+                For Each cell As DataGridViewCell In row.Cells
+                    ' Escribe el valor de la celda en el archivo binario
+                    writer.Write(Convert.ToDouble(cell.Value))
+                Next
+            Next
+
+            writer.Close()
+            rutad.Close()
+        End If
+    End Sub
+
+    Private Sub btnEnviar_Click(sender As Object, e As EventArgs) Handles btnEnviar.Click
+        dgvCalificaciones.Rows.Add(nud1.Value.ToString(), nud2.Value.ToString(), nud3.Value.ToString(), nud4.Value.ToString(), nud5.Value.ToString(), nud6.Value.ToString())
+        btnEnviar.Enabled = False
+    End Sub
+End Class
